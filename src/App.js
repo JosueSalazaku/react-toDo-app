061c5b3dc21ddf460cbddbc33b2b45876428e1b4
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-19 12:45:05 +0100

    Create FooterComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import HeaderComponents from '/Users/josuesalazaku/Developer/BeCode/Summerpastures/react-todo-app/src/Components/HeaderComponents';
+import FooterrComponents from '/Users/josuesalazaku/Developer/BeCode/Summerpastures/react-todo-app/src/Components/FooterComponents';
+
 
 function App() {
   return (
     <div className="App h-screen bg-zinc-900">
         <HeaderComponents/>
-      <main className='flex flex-row h-full'>
+      <main className='flex flex-row justify-center items-center h-full'>
       <h1 className='text-white text-6xl'>My to do's</h1>
-       </main>
+      </main>
+      <FooterrComponents/>
 
     </div>
   );
---
+++ src/Components/FooterComponents.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <footer className="h-4 w-screen flex justify-center items-center">
+        <p>Made by josuesalazaku 2024</p>
+      </footer>
+    );
+  }
+  
+  export default App;
+  
