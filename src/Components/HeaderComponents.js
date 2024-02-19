a539ad7384edf2fb1f2c4973e13d1ae92062ba45
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-19 12:26:38 +0100

    Create HeaderComponent

---
+++ src/Components/HeaderComponents.js
@@ -0,0 +1,14 @@
+function App() {
+    return (
+      <header className="App-header">
+        <ul className="flex felx-row justify-around mt-10">
+          <li>Logo</li>
+          <li>Todo's</li>
+          <li>Logs's</li>
+        </ul>
+      </header>
+    );
+  }
+  
+  export default App;
+  
